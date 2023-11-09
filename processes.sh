5cb2e535c3dbe80ca1cfafd9310f7c7f4eea1780
cdragonzfr <146982306+cdragonzfr@users.noreply.github.com>
2023-11-09 09:10:58 -0500

    Update processes.sh

--- processes.sh
+++ processes.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
 
-# Get listening TCP ports with process and user information
-netstat -anp | grep -i listen | awk '/^tcp/ || /^tcp6/ {
-    # Extract IP:Port
-    split($4, ip_port, ":");
-    ipPort = ip_port[1]":"ip_port[2];
-
-    # Extract the process ID and process name
-    split($7, pid_proc, "/");
-    pid = pid_proc[1];
-    processName = (pid == "-" ? "-" : pid_proc[2]);
-
-    # Get user context for the process if process ID is available
-    if (pid != "-") {
-        cmd = "ps -o user= -p " pid;
-        cmd | getline userContext;
-        close(cmd);
-    } else {
-        userContext = "-";
-    }
-
-    # Print the formatted output
-    print userContext, ipPort, processName;
-}'
+# List all running processes with user context, process ID, and command
+ps aux | awk '{print $1, $2, $11}' | tail -n +2
