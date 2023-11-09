430aac5461d8ca5464aae7c3cdda72078c4061fb
cdragonzfr <146982306+cdragonzfr@users.noreply.github.com>
2023-11-09 10:00:01 -0500

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Enumerate /home directory for user profile directories
-# Output: Directory Name | Permissions | Owner
+# Output: Directory Name | Permissions | Owner | Group
 ls -l /home | grep '^d' | awk '{
-    print $9 "|" $1 "|" $3
+    print $9 "|" $1 "|" $3 "|" $4
 }'
