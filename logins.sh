fab01677181950901c5b78f6d906256d82fb2381
cdragonzfr <146982306+cdragonzfr@users.noreply.github.com>
2023-11-09 23:50:02 -0500

    Update logins.sh

--- logins.sh
+++ logins.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Script to list the last logins in the past 30 days
+# Script to list the last logins in the past 30 days excluding 'nobody' accounts
 # Output format: Account Name | Session
-
-# Current date in seconds since the epoch
-current_date=$(date +%s)
 
 # Loop through the output of the `last` command
 last | while read -r line; do
@@ -13,7 +10,10 @@ last | while read -r line; do
         user=${BASH_REMATCH[1]}
         session=${BASH_REMATCH[2]}
 
-        # Print the user and session information
-        echo "$user | $session"
+        # Skip 'nobody' accounts
+        if [[ $user != "nobody" ]]; then
+            # Print the user and session information
+            echo "$user | $session"
+        fi
     fi
 done
