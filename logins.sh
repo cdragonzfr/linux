8982265ba4c07d9910f4c51196eead0ef8ae16ed
cdragonzfr <146982306+cdragonzfr@users.noreply.github.com>
2023-11-09 23:37:11 -0500

    Update logins.sh

--- logins.sh
+++ logins.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Script to list the last logins in the past 30 days
-# Output format: Account Name | Process Date | Time
+# Script to list the last logins in the past 30 days including the session
+# Output format: Account Name | Session | Process Date | Time
 
 # Current date in seconds since the epoch
 current_date=$(date +%s)
 
 # Loop through the output of the `last` command
 last | while read -r line; do
-    # Extract the username, login date, and time
-    if [[ $line =~ ^([a-zA-Z0-9_\-\.]+)\ +pts\/[0-9]+ ]]; then
+    # Extract the username, session, login date, and time
+    if [[ $line =~ ^([a-zA-Z0-9_\-\.]+)\ +([a-zA-Z0-9\/]+) ]]; then
         user=${BASH_REMATCH[1]}
-        login_date=$(echo $line | awk '{print $4, $5, $6}')
-        login_time=$(echo $line | awk '{print $7}')
+        session=${BASH_REMATCH[2]}
+        login_date=$(echo $line | awk '{print $5, $6, $7}')
+        login_time=$(echo $line | awk '{print $8}')
 
         # Convert login date to seconds since the epoch
         login_date_seconds=$(date -d "$login_date" +%s)
@@ -22,7 +23,7 @@ last | while read -r line; do
 
         # If the difference is less than or equal to 30 days, print the information
         if [ $difference -le 30 ]; then
-            echo "$user | $login_date | $login_time"
+            echo "$user | $session | $login_date | $login_time"
         fi
     fi
 done
