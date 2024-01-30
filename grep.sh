5c1d1e1da13ab8c10767926564ede39a4b30bcc0
cdragonzfr <146982306+cdragonzfr@users.noreply.github.com>
2024-01-30 11:50:08 -0500

    Update grep.sh

--- grep.sh
+++ grep.sh
@@ -1,3 +1,9 @@
 grep -oP 'y=\d+/m=\d+/d=\d+/h=\d+|blob_name="[^"]+"' your_log_file.log | \
 awk 'NR%2{printf "%s ",$0;next;}1' | \
 sort -k3,3n -k5,5n -k7,7n -k9,9n
+
+
+grep -oP '^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}|blob_name="[^"]+"' your_log_file.log | \
+awk 'NR%2{printf "%s ",$0;next;}1' | \
+sort -k1,1n -k2,2n
+
